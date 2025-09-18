cc4b8426915515dc71c2b858d73bf91df9ade6d6
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-18 20:04:30 +0100

    task 3 complete

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,10 @@
+import { pillProps } from "@/interfaces";
+const Pill: React.FC<pillProps> = ({ title }) => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
+  )
+}
+
+export default Pill;
