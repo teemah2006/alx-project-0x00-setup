c220f813844b5d08c7b85fbc97adf7f6ccd78366
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-18 21:02:50 +0100

    task 6 complete

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,6 +7,7 @@ const Landing: React.FC =  () => {
       <Card />
       <Button title="Click Me" styles="rounded-sm btn-sm"/>
       <Button title="Click Me" styles="rounded-md btn-md"/>
+      <Button title="Click Me" styles="rounded-lg btn-md"/>
       <Button title="Click Me" styles="rounded-full btn-lg"/>
     </div>
   )
